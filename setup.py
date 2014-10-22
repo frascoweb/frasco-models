d7d841f269a39f0a04b1ee909951dab16c162f2c
maximebf <maxime.bouroumeau@gmail.com>
2014-10-23 00:37:19 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from setuptools import setup, find_packages
 
 
-def desc():
-    with open("README.md") as f:
-        return f.read()
-
-def reqs():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
 setup(
     name='frasco-models',
     version='0.1',
@@ -17,11 +9,13 @@ setup(
     author='Maxime Bouroumeau-Fuseau',
     author_email='maxime.bouroumeau@gmail.com',
     description="ORM for Frasco",
-    long_description=desc(),
     packages=find_packages(),
+    zip_safe=False,
     platforms='any',
-    install_requires=reqs() + [
-        'frasco',
-        'persistpy'
+    install_requires=[
+        # 'frasco',
+        # 'persistpy',
+        'inflection',
+        'flask-mongoengine>=0.7'
     ]
 )
