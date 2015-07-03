56850f1a6f12d480fae84824305b93647f7cf112
maximebf <maxime.bouroumeau@gmail.com>
2015-07-03 11:46:11 +0200

    added missing package data

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='frasco-models',
-    version='0.3',
+    version='0.3.1',
     url='http://github.com/frascoweb/frasco-models',
     license='MIT',
     author='Maxime Bouroumeau-Fuseau',
     author_email='maxime.bouroumeau@gmail.com',
     description="ORM for Frasco",
     packages=find_packages(),
+    package_data={
+        'frasco_models': [
+            'admin/templates/admin/models_default/*.html',
+            '*.html']
+    },
     zip_safe=False,
     platforms='any',
     install_requires=[
